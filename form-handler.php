4eae41e9c65ad90a1b7408802774de64d0043e7b
Utkarsh Krishna <utkarshmark14@gmail.com>
2023-07-07 21:10:51 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@yourwebsite.com';
+$email_from = 'mesmerizer2023@gmail.com';
 
 $email_subject = 'New Form Submission';
 
