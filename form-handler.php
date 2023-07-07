8ff7f017e5333cdf0b6b93ebc93e9a068c8a6600
Utkarsh Krishna <utkarshmark14@gmail.com>
2023-07-07 22:00:16 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,26 +1,29 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-$email_from = 'mesmerizer2023@gmail.com';
+    $to = 'utkarshkrishna04@gmail.com'; // Replace with your own email address
 
-$email_subject = 'New Form Submission';
+    $email_subject = 'New Form Submission';
+    $email_body = "User Name: $name\n".
+                  "User Email: $visitor_email\n".
+                  "Subject: $subject\n".
+                  "User Message: $message\n";
 
-$email_body = "User Name: $name.\n".
-              "User Email: $visitor_email.\n".
-              "Subject: $subject.\n".
-              "User Message: $message .\n".;
+    $headers = "From: $visitor_email\r\n".
+               "Reply-To: $visitor_email\r\n";
 
-
-$to = 'utkarshkrishna04@gmail.com';
-
-$headers = "From: $email_from \r\n";
-
-$headers .= "Reply-To: $visitor_email \r\n";
-
-mail($to,$email_subject,$email_body,$headers);
-
-header("Location: contact.html");
+    if (mail($to, $email_subject, $email_body, $headers)) {
+        // Successful submission, redirect to the contact page
+        header("Location: contact.html");
+        exit();
+    } else {
+        // Error sending email, redirect to an error page
+        header("Location: error.html"); // Replace with your error page URL
+        exit();
+    }
+}
 ?>
